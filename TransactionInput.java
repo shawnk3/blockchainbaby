5ce441f0c0527b33ddcac09c6f4b486a0d8a878e
shawnk3 <32775598+shawnk3@users.noreply.github.com>
2019-07-17 13:57:22 -0400

    fix

--- TransactionInput.java
+++ TransactionInput.java
@@ -4,7 +4,7 @@ public class TransactionInput {
     public String TransactionOutputID;//TransactionOutput -> transactionId
     public TransactionOutput UTXO;//unspent transaction TransactionOutput
 
-    public TransactionOutput(String TransactionOutputID){
+    public TransactionInput(String TransactionOutputID){
         this.TransactionOutputID = TransactionOutputID;
     }
 }
