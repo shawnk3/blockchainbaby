970599544d35c4f500f52553d750e93908ef03e6
shawnk3 <32775598+shawnk3@users.noreply.github.com>
2019-07-19 11:31:10 -0400

    updates

--- TransactionOutput.java
+++ TransactionOutput.java
@@ -5,12 +5,13 @@ public class TransactionOutput{
     public PublicKey recipient;//new owner of coins
     public float value;// value user owns
     public String parentTransactionId;//the id of the transaction this output was created 
+    public StringUtil tool = new StringUtil();
 
     public TransactionOutput(PublicKey recipient,float value, String parentTransactionId){
         this.recipient = recipient;
         this.value = value;
         this.parentTransactionId = parentTransactionId;
-        this.id = StringUtil.applySha256(StringUtil.getStringfromKey(recipient)+Float.toString(value)+parentTransactionId);
+        this.id = tool.applySha256(tool.getStringfromKey(recipient)+Float.toString(value)+parentTransactionId);
     }
     
     public boolean isMine(PublicKey key){
